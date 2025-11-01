924fa0db3662ef2f6097224bbde58fdfe604aeed
pokedot <110330779+pokedot@users.noreply.github.com>
2025-11-01 20:32:55 +0800

    Add tests for case sensitivity

--- src/test/java/seedu/address/model/person/NameTest.java
+++ src/test/java/seedu/address/model/person/NameTest.java
@@ -49,6 +49,9 @@ public class NameTest {
         // same values -> returns true
         assertTrue(name.equals(new Name("Valid Name")));
 
+        // same values but lowercase -> returns true
+        assertTrue(name.equals(new Name("valid name")));
+
         // same object -> returns true
         assertTrue(name.equals(name));
 
